ff948a8b5365231323862ef3a19cff8243c18252
Ayan Mohammed <6030437@mborijnland.nl>
2024-12-05 22:57:13 +0100

    header 3.0

--- PHP/header.php
+++ PHP/header.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    <link rel="stylesheet" type="text/css" href="CSS\style.css">
     <title>Mbocinemas</title>
 </head>
 <header>
-    <h1>🎬Mbocinemas</h1>
+<a href="index.php"><h2>🎬Mbocinemas</h2></a>
+    <a href="PHP/films.php"><h3>Films</h3></a>
+    <a href="PHP/reserveringen.php"><h4>Mijn Reserveringen</h4></a>
+    <a href="PHP/contact.php"><h4>Contact</h4></a>
+    <a href="PHP/aboutus.php"><h4>About Us</h4></a>
+    <input type="text" placeholder="Film Zoeken">
+    <a href="PHP/inlogpagina.php"><h5>Inloggen</h5></a>
 </header>
 <body>
